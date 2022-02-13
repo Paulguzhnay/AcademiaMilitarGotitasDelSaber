cb4bec78aa024b5b11bb607b7d23be29eb225ebd
Paulguzhnay <69018647+Paulguzhnay@users.noreply.github.com>
2022-02-12 19:49:59 -0500

    Servicio Rest Desarrollado
    Se puede consultar las notas del cliente por medio de rest

---
+++ AcademiaMilitar/src/main/java/ec/edu/ups/service/AppPath.java
@@ -0,0 +1,11 @@
+package ec.edu.ups.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rs")
+public class AppPath extends Application{
+
+	
+	
+}
